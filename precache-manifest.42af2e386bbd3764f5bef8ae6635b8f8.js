8f92000446f001eb3459802f55a80672a7300056
Greg Answer <greganswer@gmail.com>
2019-09-10 21:11:07 -0400

    Updates

---
+++ precache-manifest.42af2e386bbd3764f5bef8ae6635b8f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef0d6e6d69d5d1e52992080637c2c0e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ab7d4442e666156375a9",
+    "url": "/monsters-rolodex/static/css/main.05da3697.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "ab7d4442e666156375a9",
+    "url": "/monsters-rolodex/static/js/main.24f4aa75.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
